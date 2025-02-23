66bd6533a695a1849e40713f02aefd745044cc01
rashik07 <saif67090707@gmail.com>
2025-02-23 18:28:51 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ async function main() {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`app is listening on port ${config.port}`);
+      console.log(`\x1b[32mApp is listening on port ${config.port}\x1b[0m`);
     });
   } catch (err) {
     console.log(err);
