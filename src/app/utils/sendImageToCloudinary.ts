048e9d1489771c7c3a8f4e3947305bd668c3a2c7
rashik07 <saif67090707@gmail.com>
2025-04-18 11:29:45 +0600

    image upload error

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -1,4 +1,5 @@
 import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
 import multer from 'multer';
 import config from '../config';
 
@@ -10,21 +11,38 @@ cloudinary.config({
 
 export const sendImageToCloudinary = (
   imageName: string,
-  buffer: Buffer,
+  path: string,
 ): Promise<Record<string, unknown>> => {
   return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload_stream(
+    cloudinary.uploader.upload(
+      path,
       { public_id: imageName.trim() },
       function (error, result) {
         if (error) {
           reject(error);
         }
         resolve(result as UploadApiResponse);
+        // delete a file asynchronously
+        fs.unlink(path, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('File is deleted.');
+          }
+        });
       },
-    ).end(buffer);
+    );
   });
 };
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, process.cwd() + '/uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
 
 export const upload = multer({ storage: storage });
