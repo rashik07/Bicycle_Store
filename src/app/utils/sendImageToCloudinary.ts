f29a0070b6abcfb30320f4522c5f0cacd581b204
rashik07 <saif67090707@gmail.com>
2025-04-18 12:05:46 +0600

    image upload error slove

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -2,6 +2,7 @@ import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
 import multer from 'multer';
 import config from '../config';
+import path from 'path';
 
 cloudinary.config({
   cloud_name: config.cloudinary_cloud_name,
@@ -36,13 +37,53 @@ export const sendImageToCloudinary = (
 };
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, process.cwd() + '/uploads/');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
-  },
-});
+    destination: function (req, file, cb) {
+        cb(null, path.join(process.cwd(), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
 
 export const upload = multer({ storage: storage });
+
+
+// import multer from "multer"
+// import path from "path"
+// import fs from 'fs'
+// import { v2 as cloudinary } from 'cloudinary';
+// import { ICloudinaryResponse, IFile } from "../interface/files"
+
+
+// cloudinary.config({
+//     cloud_name: 'dbgrq28js',
+//     api_key: '173484379744282',
+//     api_secret: 'eHKsVTxIOLl5oaO_BHxBQWAK3GA'
+// });
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(process.cwd(), 'uploads'))
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.originalname)
+//     }
+// })
+
+// export const upload = multer({ storage: storage })
+
+// export const sendImageToCloudinary = async (file: IFile): Promise<ICloudinaryResponse | undefined> => {
+//     return new Promise((resolve, reject) => {
+//         cloudinary.uploader.upload(file.path,
+//             (error: Error, result: ICloudinaryResponse) => {
+//                 fs.unlinkSync(file.path)
+//                 if (error) {
+//                     reject(error)
+//                 }
+//                 else {
+//                     resolve(result)
+//                 }
+//             })
+//     })
+// };
+
